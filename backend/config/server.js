4fb2f2b23647b2b8bd5d1dde3b377bcd284a122b
Timothee Regis <registimothee@gmail.com>
2021-02-05 13:26:24 +0100

    initial commit with strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bf5eb0ff9a3882976b089f8327e899d6'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4bd916ec-2d5f-4900-9c37-ac3fb26a0158'
+};
